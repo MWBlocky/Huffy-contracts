668476f1b5eb485207526c0e06185f440ff4c2b7
Michał Walczak <michal.walczak@arianelabs.com>
2025-10-16 10:03:00 +0200

    docs: add reference to Saucerswap Router ABI link and method signature
    Signed-off-by: Michał Walczak <michal.walczak@arianelabs.com>

--- src/Treasury.sol
+++ src/Treasury.sol
@@ -246,6 +246,8 @@ contract Treasury is AccessControl, ReentrancyGuard {
 
 /**
  * @notice Minimal Saucerswap Router interface
+ * https://hashscan.io/mainnet/contract/0.0.3045981/abi
+ * 12. function swapExactTokensForTokens(uint256 amountIn, uint256 amountOutMin, address[] path, address to, uint256 deadline) returns (uint256[] amounts) NONPAYABLE 0x38ed1739
  */
 interface ISaucerswapRouter {
     function swapExactTokensForTokens(
